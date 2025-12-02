8e8a2c6640164757539e436bcb8e5b31409e0674
Pratibha573 <sypratibha63@gmail.com>
2025-12-02 12:10:13 +0530

    Create test_student.py

---
+++ test_student.py
@@ -0,0 +1,10 @@
+from student import Student_details
+def test_student_details():
+    expected_output=(
+    "Student Name:Alice\n"
+    "Student ID:E1001\n"
+    "Course:bca\n"
+    "Year:2\n"
+    "Fees:55000"
+    )
+    assert student_details("Alice","E1001","bca",2,55000)==expected_output
